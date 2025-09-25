aff1d52d40ed11fc22529b823c8bd53ac0ac2b50
mwcastellan <91078828+mwcastellan@users.noreply.github.com>
2025-09-25 18:21:41 -0300

    Update Register01.sol

--- modulo1/Register01.sol
+++ modulo1/Register01.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+//pragma solidity 0.8.26;
+pragma solidity >0.8.0;
 
+contract Register01 {
+    string private storedInfo;
+
+    function setInfo(string memory myInfo) external {
+        storedInfo = myInfo;
+    }
+
+    function getInfo() external view returns (string memory) {
+        return storedInfo;
+    }
+}
