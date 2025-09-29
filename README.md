55c9bf132e3180da780fb38ca4150483354078a5
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2025-09-29 18:43:14 -0300

    mwcastellan

--- README.md
+++ README.md
@@ -6,5 +6,13 @@ MODULO1
 
 MODULO 2
 1) Proyecto1.sol
-   Sepolia Tesnet. Contract 0xF7bEeCb09E3dcFDC7238fA3E3E949AF910fF0D02 
+   Sepolia Tesnet. Contract 0x1B97d4D3e6cd88d4303418a6b6024bd5EE0171B5
+   Address 0x3157dFFf5cfb26728E265a73E3f3bA8Ed3150AF0
+
+2) Proyecto2.sol
+   Sepolia Tesnet. Contract 0x246A593fd5437639aF267c9D766A284a18ee4909
+   Address 0x3157dFFf5cfb26728E265a73E3f3bA8Ed3150AF0
+
+3) Proyecto3.sol
+   Sepolia Tesnet. contract address	0x464a1acc3657bc3ac02da4aef9c5c1690eb613ad
    Address 0x3157dFFf5cfb26728E265a73E3f3bA8Ed3150AF0
--- modulo2/Proyecto2.sol
+++ modulo2/Proyecto2.sol
@@ -2,7 +2,6 @@
 pragma solidity >0.8.0;
 
 contract ToDoList {
-
     enum State {
         SinHacer,
         Completado
@@ -18,13 +17,23 @@ contract ToDoList {
     Tarea[] public tarea; // tarea[indice]
     uint256 private nextIndex;
 
-    event TaskAdded(uint256 indexed index,string indexed description, uint256 creationTime);
-    event TaskedStatusChanged(uint256 indexed index,string indexed description,string indexed newStatus);
+    event TaskAdded(
+        uint256 indexed index,
+        string indexed description,
+        uint256 creationTime
+    );
+    event TaskedStatusChanged(
+        uint256 indexed index,
+        string indexed description,
+        string indexed newStatus
+    );
 
     function setTarea(string calldata _description) external {
         uint256 _lastIndex = nextIndex++;
-        tarea.push(Tarea(_description,block.timestamp,_lastIndex,State.SinHacer));
-        emit TaskAdded(_lastIndex,_description, block.timestamp);
+        tarea.push(
+            Tarea(_description, block.timestamp, _lastIndex, State.SinHacer)
+        );
+        emit TaskAdded(_lastIndex, _description, block.timestamp);
     }
 
     function getTareas() external view returns (Tarea[] memory) {
@@ -33,10 +42,17 @@ contract ToDoList {
 
     function eliminarTarea(string calldata _descripcion) external {
         uint256 len = tarea.length;
-        for(uint256 i; i<len;) {
-            if(keccak256(bytes(tarea[i].description)) == keccak256(bytes(_descripcion))) {
-                emit TaskedStatusChanged(tarea[i].index,tarea[i].description,"eliminado");
-                tarea[i] = tarea[len-1];
+        for (uint256 i; i < len; ) {
+            if (
+                keccak256(bytes(tarea[i].description)) ==
+                keccak256(bytes(_descripcion))
+            ) {
+                emit TaskedStatusChanged(
+                    tarea[i].index,
+                    tarea[i].description,
+                    "eliminado"
+                );
+                tarea[i] = tarea[len - 1];
                 tarea.pop();
                 break;
             }
@@ -48,9 +64,16 @@ contract ToDoList {
 
     function completarTarea(string calldata _descripcion) external {
         uint256 len = tarea.length;
-        for(uint256 i; i<len;) {
-            if(keccak256(bytes(tarea[i].description)) == keccak256(bytes(_descripcion))) {
-                emit TaskedStatusChanged(tarea[i].index,tarea[i].description,"Completado");
+        for (uint256 i; i < len; ) {
+            if (
+                keccak256(bytes(tarea[i].description)) ==
+                keccak256(bytes(_descripcion))
+            ) {
+                emit TaskedStatusChanged(
+                    tarea[i].index,
+                    tarea[i].description,
+                    "Completado"
+                );
                 tarea[i].state = State.Completado;
                 break;
             }
@@ -59,5 +82,4 @@ contract ToDoList {
             }
         }
     }
-
 }
